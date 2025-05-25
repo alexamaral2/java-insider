4dc7a393f1ff1534f01e3f9590d454516eaba067
Alex Jr <alexjunior2018@gmail.com>
2025-05-25 14:41:19 -0300

    adicionando casting grades

---
+++ intro/src/modulo02/Casting.java
@@ -0,0 +1,16 @@
+package modulo02;
+
+public class Casting {
+
+	public static void main(String[] args) {
+
+		// double a = 5.0 / 2;
+
+		double a = (double) 5 / 2;
+		int b = (int) a;
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+
+}
