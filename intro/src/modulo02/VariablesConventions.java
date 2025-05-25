562739a304cb3a3adcf1c71b41f4953eef2afb08
Alex Jr <alexjunior2018@gmail.com>
2025-05-25 01:00:38 -0300

    initial commit

---
+++ intro/src/modulo02/VariablesConventions.java
@@ -0,0 +1,18 @@
+package modulo02;
+
+public class VariablesConventions {
+
+	public static void main(String[] args) {
+		int age;
+		int $a; // não recomendado
+		int _age; // não recomendado
+		int age2;
+		int Age; // não recomendado
+		int AGE; // não recomendado
+
+		String firstName; // OK!
+		String lastName; // OK!
+
+	}
+
+}
