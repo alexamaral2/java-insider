f47db149e1c6203792dda27eb15dd43f101f60f5
Alex Junior <alexjunior2018@gmail.com>
2025-05-27 13:50:13 -0300

    adicionando exercicios

---
+++ intro/src/modulo02/DaysInMonths.java
@@ -0,0 +1,20 @@
+package modulo02;
+
+public class DaysInMonths {
+
+	public static void main(String[] args) {
+
+		int month = 3;
+		int numDays;
+
+		if (month == 2) {
+			numDays = 28;
+		} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			numDays = 31;
+		} else {
+			numDays = 30;
+		}
+
+		System.out.println("O mês " + month + " possui " + numDays + " dias.");
+	}
+}
