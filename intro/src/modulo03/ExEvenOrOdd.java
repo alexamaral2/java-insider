f7404e8fc71d29fbdae2c462f1a4f36cfd635b1b
Alex Junior <alexjunior2018@gmail.com>
2025-05-28 14:02:03 -0300

    adicionando exercicio par

---
+++ intro/src/modulo03/ExEvenOrOdd.java
@@ -0,0 +1,15 @@
+package modulo03;
+
+public class ExEvenOrOdd {
+
+	public static void main(String[] args) {
+		int n = 345;
+
+		if (n % 2 == 0) {
+			System.out.println("O número " + n + " é par");
+		} else {
+			System.out.println("O número " + n + " é impar");
+		}
+	}
+
+}
