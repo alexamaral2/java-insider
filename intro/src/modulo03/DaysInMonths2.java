9bf743c7a66d7fb5aa5b5fc68e3fc72d64765c33
Alex Jr <alexjunior2018@gmail.com>
2025-05-28 00:30:30 -0300

    adicionando exemplo

---
+++ intro/src/modulo03/DaysInMonths2.java
@@ -0,0 +1,29 @@
+package modulo03;
+
+public class DaysInMonths2 {
+
+	public static void main(String[] args) {
+		// Ano Bissexto
+		// 1. Divisivel por 400; ou
+		// 2. Divisivel por 4 e não divisivel por 100
+
+		int month = 2;
+		int year = 800;
+		int numDays;
+
+		if (month == 2) {
+			if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+				numDays = 29;
+			} else {
+				numDays = 28;
+			}
+		} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+			numDays = 31;
+		} else {
+			numDays = 30;
+		}
+
+		System.out.println("O mês " + month + " possui " + numDays + " dias.");
+	}
+
+}
---
+++ intro/src/modulo03/VariablesRules.java
@@ -0,0 +1,19 @@
+package modulo03;
+
+public class VariablesRules {
+
+	public static void main(String[] args) {
+
+		int n;
+		boolean o = true;
+
+		if (o) {
+			n = 1;
+		} else {
+			n = 2;
+		}
+
+		System.out.println(n);
+	}
+
+}
