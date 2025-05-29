c286d18265d4007179fd86927b364eec8c83bd01
Alex Junior <alexjunior2018@gmail.com>
2025-05-29 15:58:41 -0300

    adicionando exercicio dado

---
+++ intro/src/modulo03/ExRollingDice.java
@@ -0,0 +1,22 @@
+package modulo03;
+
+public class ExRollingDice {
+
+	public static void main(String[] args) {
+
+		int d1 = (int) (Math.random() * 6) + 1;
+		int d2 = (int) (Math.random() * 6) + 1;
+
+		System.out.println("D1 = " + d1);
+		System.out.println("D1 = " + d2);
+
+		int sum = d1 + d2;
+
+		if (d1 == d2) {
+			sum = sum * 2;
+		}
+
+		System.out.println("Casas = " + sum);
+	}
+
+}
