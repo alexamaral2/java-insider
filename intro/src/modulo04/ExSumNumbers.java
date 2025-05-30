9f9a137bcbf0a1c40c48ed3d6f23901a6c38b5c1
Alex Junior <alexjunior2018@gmail.com>
2025-05-30 16:30:46 -0300

    adicionando exercicio

---
+++ intro/src/modulo04/ExSumNumbers.java
@@ -0,0 +1,17 @@
+package modulo04;
+
+public class ExSumNumbers {
+
+	public static void main(String[] args) {
+
+		int n = 3;
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			sum += i;
+		}
+
+		System.out.println("A soma é: " + sum);
+	}
+
+}
