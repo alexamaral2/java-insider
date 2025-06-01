5e2ee4b9aeaa07aa480d5a0cffd99bd6e5210cf9
Alex Jr <alexjunior2018@gmail.com>
2025-05-31 23:16:52 -0300

    adicionando exercicio

---
+++ intro/src/modulo04/ExFibonacci.java
@@ -0,0 +1,22 @@
+package modulo04;
+
+public class ExFibonacci {
+
+	public static void main(String[] args) {
+
+		int a = 1;
+		int b = 1;
+
+		System.out.println(a);
+		System.out.println(b);
+
+		for (int i = 3; i <= 15; i++) {
+			int p = a + b;
+			System.out.println(p);
+
+			b = a;
+			a = p;
+		}
+	}
+
+}
