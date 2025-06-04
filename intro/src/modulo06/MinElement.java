3efb57cee9440f837580954d6aa469cf5cda816d
Alex Junior <alexjunior2018@gmail.com>
2025-06-04 13:55:47 -0300

    adicionando exemplos

---
+++ intro/src/modulo06/MinElement.java
@@ -0,0 +1,34 @@
+package modulo06;
+
+public class MinElement {
+
+	public static void main(String[] args) {
+
+		System.out.println("inicio");
+
+		int[] arr = { 1, 6, 9 };
+
+		int[] arr2 = new int[2];
+		arr2[0] = 5;
+		arr2[1] = 4;
+
+		findMin(arr);
+		findMin(new int[] { -1, 6, -5 });
+		findMin(arr2);
+
+		System.out.println("fim");
+
+	}
+
+	static void findMin(int[] array) {
+		int min = Integer.MAX_VALUE;
+
+		for (int e : array) {
+			if (e < min) {
+				min = e;
+			}
+		}
+		System.out.println(min);
+	}
+
+}
